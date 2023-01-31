9c5b9c779b427f2bd778bffdc69ea2165ca77e43
shounak03 <83414283+shounak03@users.noreply.github.com>
2023-01-31 12:41:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,31 +21,31 @@ function makeSound(key){
 
     switch(key){
         case "w":
-        var a = new Audio('sounds/crash.mp3');
+        var a = new Audio('crash.mp3');
         a.play(); break;
 
         case "a":
-        var a = new Audio('sounds/kick-bass.mp3');
+        var a = new Audio('kick-bass.mp3');
         a.play(); break;
 
         case "s":
-        var a = new Audio('sounds/snare.mp3');
+        var a = new Audio('snare.mp3');
         a.play(); break;
 
         case "d":
-        var a = new Audio('sounds/tom-1.mp3');
+        var a = new Audio('tom-1.mp3');
         a.play(); break;
 
         case "j":
-        var a = new Audio('sounds/tom-2.mp3');
+        var a = new Audio('tom-2.mp3');
         a.play(); break;
 
         case "k":
-        var a = new Audio('sounds/tom-3.mp3');
+        var a = new Audio('tom-3.mp3');
         a.play(); break;
 
         case "l":
-        var a = new Audio('sounds/tom-4.mp3');
+        var a = new Audio('tom-4.mp3');
         a.play(); break;
 
     }
